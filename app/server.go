b8f2afc457f2ab6696bd92e1bdccd56afcd9a0c6
Abhishek Sah <abhi.sah.97@gmail.com>
2024-08-10 18:01:32 +0530

    update resp

--- app/server.go
+++ app/server.go
@@ -52,7 +52,7 @@ func handleConnection(conn net.Conn) error {
 		httpResp := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(toEcho), toEcho)
 		conn.Write([]byte(httpResp))
 	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n\r\n"))
 	}
 	return nil
 }
