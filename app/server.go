a2a7aec7ee2820d30cc11cfdc1ddf6d625904cd2
Abhishek Sah <abhi.sah.97@gmail.com>
2024-08-17 11:56:58 +0530

    return files

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -11,6 +12,9 @@ import (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
+
+	directory := flag.String("directory", "/tmp", "directory for streaming files")
+	flag.Parse()
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -24,7 +28,7 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConnection(conn)
+		go handleConnection(conn, *directory)
 	}
 }
 
@@ -32,7 +36,12 @@ func prepHttPResp(arg string) string {
 	return fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(arg), arg)
 }
 
-func handleConnection(conn net.Conn) error {
+func prepOctetHttpResp(bytes []byte) string {
+	return fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(string(bytes)), string(bytes))
+}
+
+func handleConnection(conn net.Conn, directory string) error {
+	defer conn.Close()
 	buffer := make([]byte, 1024)
 	_, err := conn.Read(buffer)
 	if err != nil {
@@ -55,13 +64,24 @@ func handleConnection(conn net.Conn) error {
 		userHeaderAgentValue := strings.Split(splittedUserAgent, "\r\n")[0]
 		conn.Write([]byte(prepHttPResp(userHeaderAgentValue)))
 	default:
-		match, _ := regexp.MatchString("/echo/([a-z]+)", path)
-		if match {
+		echoMatch, _ := regexp.MatchString("/echo/([a-z]+)", path)
+		filesMatch, _ := regexp.MatchString("/files/([a-z]+)", path)
+		if echoMatch {
 			toEcho := path[len("/echo/"):]
 			conn.Write([]byte(prepHttPResp(toEcho)))
+			return nil
+		}
+		if filesMatch {
+			fileName := path[len("/file/"):]
+			fileBytes, err := os.ReadFile(fmt.Sprintf("%s/%s", directory, fileName))
+			if err != nil {
+				conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+				return nil
+			}
+			conn.Write([]byte(prepOctetHttpResp(fileBytes)))
+			return nil
 		}
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-	conn.Close()
 	return nil
 }
